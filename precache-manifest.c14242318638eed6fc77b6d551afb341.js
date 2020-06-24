dbc588a34d565a87c853b8c3b5198a61e4ec7f3a
YANNIEYEUNG <K_T_DINO@HOTMAIL.COM>
2020-06-24 17:12:13 +0800

    Updates

---
+++ precache-manifest.c14242318638eed6fc77b6d551afb341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc904d663ce3363293b601eabdf8d2b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa039a4d71daaf6276a7",
+    "url": "/static/css/main.909a988f.chunk.css"
+  },
+  {
+    "revision": "e7ffb48a20691a7692ca",
+    "url": "/static/js/2.d8f90681.chunk.js"
+  },
+  {
+    "revision": "4d6ab1395afd48c38ab429e9c82a1cee",
+    "url": "/static/js/2.d8f90681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa039a4d71daaf6276a7",
+    "url": "/static/js/main.477ab8b3.chunk.js"
+  },
+  {
+    "revision": "bbfdc15ddd9626259bd4",
+    "url": "/static/js/runtime-main.0b58f76c.js"
+  }
+]);
